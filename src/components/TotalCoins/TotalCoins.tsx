b5138c4aed5bc1b79aa0c8d1a25a5694b97ded5e
Adrian <Adrian.b.persen@gmail.com>
2026-09-26 13:54:56 +0200

    Stylet GamePage og forbedret responsivt design

--- src/components/TotalCoins/TotalCoins.tsx
+++ src/components/TotalCoins/TotalCoins.tsx
@@ -1,12 +1,16 @@
 import { useGameStore } from "../../store/useGameStore";
+import "./TotalCoins.css";
 
 /* Viser hvor mange coins den valgte spilleren har */
 export default function TotalCoins() {
     const currentPlayer = useGameStore((state) => state.currentPlayer);
 
     return (
-        <div>
-            <p>Coins: {currentPlayer ? currentPlayer.coins : "-"}</p>
+        <div className="total-coins">
+            <p>Coins:</p> 
+            <p className="coin-amount">
+              {currentPlayer ? currentPlayer.coins : "-"}
+              </p>
         </div>
     )
 
